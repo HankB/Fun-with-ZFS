3124cc8352f26ac4c0c39ab6c610d1a659f368b4
Hank Barta <hbarta@gmail.com>
2021-02-06 21:44:51 -0600

    the script

---
+++ bulk-transfer/bulk-xfer.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# probably skip these if copy/past commands to shell
+set -o xtrace   # display commands as they are executed
+set -o errexit  # bail on any errors
+set -o nounset  # bail if undeclared variable
+
+# create disk files to use as physical devices
+starting_point=$(pwd)
+truncate -s 64MiB ./fakedisk_source
+truncate -s 64MiB ./fakedisk_destination
+
+# create pools using these devices
+zpool create source "$starting_point"/fakedisk_source
+zpool create destination "$starting_point"/fakedisk_destination
+
+# create filesystems on source pool
+zfs create source/source
+zfs create source/source/foo
+zfs create source/source/baz
+zfs create source/source/baz/bar
+
+echo "open shell to examine results so far (exit or <ctrl>D to proceed)"
+/usr/bin/env bash
+
+# and populate with some files
+cd /source/source
+dd bs=1M seek=1 of=source-source count=0
+cd baz
+dd bs=1M seek=1 of=source-source-baz count=0
+cd bar
+dd bs=1M seek=1 of=source-source-baz-bar count=0
+tree ../..
+
+# snapshot and create another file
+zfs snap -r source@first
+cd ../../foo
+dd bs=1M seek=1 of=source-source-foo count=0
+zfs snap -r source@second
+ls -lR /source
+zfs list -t snap -r source
+
+echo "open shell to examine results so far (exit or <ctrl>D to proceed)"
+/usr/bin/env bash
+
+# Attempt to transfer the contents of source to destination.
+# zfs send -R source@second|zfs receive -d destination # fails because filesystem destination exists
+
+# Solution suggested by comrade meowski
+# zfs send -vRw oldtank@bulk_xfer | zfs recv -eu -o mountpoint=none tank
+zfs send -vRw source@second | zfs recv -eu -o mountpoint=none destination # mapped to test pools/filesystems
+zfs list -r destination
+zfs list -r destination -t snap
+
+# set mountpoint so we can see the results in destination
+# (Will be inherited by child filesystems.)
+zfs set mountpoint=/destination/source destination/source
+tree /source /destination
+
+echo "open shell to examine results so far (exit or <ctrl>D to proceed)"
+/usr/bin/env bash
+
+# cleanup
+cd "$starting_point"
+zpool destroy -f source
+zpool destroy -f destination
+
+rm ./fakedisk_source ./fakedisk_destination
