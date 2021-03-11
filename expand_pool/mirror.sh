503fc49139d1438a81b3cf664ce2a0d15dcc4680
Hank Barta <hbarta@gmail.com>
2021-03-11 12:20:05 -0600

    attach/mirror example

---
+++ expand_pool/mirror.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# replicate A -> B/A and B -> C
+# Following initial replication, repeat the cycle uysing incremental send/recv
+
+# Modified to use `sudo` where required and `zfs allow`
+# so operations will be performed as a non-root user.
+
+# probably skip these if copy/past commands to shell
+set -o xtrace   # display commands as they are executed
+set -o errexit  # bail on any errors
+set -o nounset  # bail if undeclared variable
+
+# create disk files to use as physical devices
+starting_point=$(pwd)
+truncate -s 64MiB ./fakedisk_original
+truncate -s 64MiB ./fakedisk_add
+
+# create pools using these devices
+sudo zpool create -m "$starting_point"/original original  \
+    "$starting_point"/fakedisk_original
+
+zpool status original
+sudo zpool attach original "$starting_point"/fakedisk_original "$starting_point"/fakedisk_add
+zpool status original
+
+sudo zpool destroy -f original
+rm ./fakedisk_original ./fakedisk_add
+sudo rm -r original
